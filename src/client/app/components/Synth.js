409d4360f826047e5c1a5801acd776b80170d32b
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-11-05 11:49:49 -0700

    make synth global

--- src/client/app/components/Synth.js
+++ src/client/app/components/Synth.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Component } from 'react';
 import keymap from '../misc/keymap';
 
+let syn;
+
 class Synth extends Component {
   constructor() {
     super();
@@ -22,17 +24,19 @@ class Synth extends Component {
   componentDidMount() {
     // const dist = new Tone.Distortion(0.8).toMaster();
     // const ping = new Tone.PingPongDelay('4n', .2).toMaster();
-    // let synth = new Tone.Synth().connect(ping);
-    // synth.triggerAttackRelease('C4', '8n')
+    // syn = new Tone.Synth().connect(dist);
+    // syn.triggerAttackRelease('D5', '8n');
+    // console.log(syn);
     
   }
   playSound(e) {
-    let key = e.key;
-    console.log(keymap[key]);
-    const ping = new Tone.PingPongDelay('4n', .2).toMaster();
-    let synth = new Tone.Synth().connect(ping);
-    console.log(`${keymap[key]}${keymap.oct}`)
-    synth.triggerAttackRelease(`${keymap[key]}${keymap.oct}`, '8n')
+    //try passing the synth down as a prop
+    // let key = e.key;
+    // console.log(keymap[key]);
+    // const ping = new Tone.PingPongDelay('4n', .2).toMaster();
+    // let synth = new Tone.Synth().connect(ping);
+    // console.log(`${keymap[key]}${keymap.oct}`)
+    // synth.triggerAttackRelease(`${keymap[key]}${keymap.oct}`, '8n')
   }
   onClick() {
     
