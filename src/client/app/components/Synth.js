d9344d40966f5cc403063f367feab1f7e4aa4c5f
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-11-05 12:39:55 -0700

    created function that maps effect chain state to objects

--- src/client/app/components/Synth.js
+++ src/client/app/components/Synth.js
@@ -9,7 +9,14 @@ class Synth extends Component {
     super();
     this.state = {
       stack: [
-        "Synth"
+        {
+          name: "Synth",
+         args: []
+        },
+        {
+        name:"PingPongDelay",
+         args: ['4n', 0.9]
+        }
       ],
       synthString: '',
       keymap
@@ -22,21 +29,45 @@ class Synth extends Component {
     window.removeEventListener('keypress', this.playSound);
   }
   componentDidMount() {
-    // const dist = new Tone.Distortion(0.8).toMaster();
-    // const ping = new Tone.PingPongDelay('4n', .2).toMaster();
-    // syn = new Tone.Synth().connect(dist);
+    // let buildStr = 'new ';
+    
+    // let stackStr = this.state.stack
+    //   .map((compObj, i, a) => {
+    //     console.log()
+    //     let args = compObj.args.length ? compObj.args.join(',') : '';
+    //     if (a.length - 1 !== i) {
+    //       return `Tone.${compObj.name}(${args}).`
+    //     }
+    //     return `Tone.${compObj.name}(${args})`
+    //   }).join("");
+    let effectsArr = this.mapEffects();
+    console.log(effectsArr);
+    //how to chain stuff;
+    // const dist = new Tone.Distortion(0.8);
+    // const ping = new Tone.PingPongDelay('4n', .2);
+    // syn = new Tone.Synth().chain(dist, ping, Tone.Master);
     // syn.triggerAttackRelease('D5', '8n');
     // console.log(syn);
     
   }
+  mapEffects() {
+    return this.state.stack  
+      .map((compObj, i, a) => {
+        let args = compObj.args.length ? compObj.args.join(',') : '';
+        let name = `ef${i}`;
+        let effectObj = {};
+        effectObj[name] = `Tone.${compObj.name}(${args})`
+        return effectObj;
+      });
+  }
   playSound(e) {
     //try passing the synth down as a prop
-    // let key = e.key;
+    let key = e.key;
     // console.log(keymap[key]);
     // const ping = new Tone.PingPongDelay('4n', .2).toMaster();
     // let synth = new Tone.Synth().connect(ping);
     // console.log(`${keymap[key]}${keymap.oct}`)
-    // synth.triggerAttackRelease(`${keymap[key]}${keymap.oct}`, '8n')
+    // syn.triggerAttackRelease(`${keymap[key]}${keymap.oct}`, '8n')
   }
   onClick() {
     
